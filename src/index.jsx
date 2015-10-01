8da66e552d830470f7fe7faaa573e978aaf86c5b
Trulls <annaj9@kth.se>
2015-10-01 22:19:05 +0200

    Done entering main experiences

--- src/index.jsx
+++ src/index.jsx
@@ -11,9 +11,10 @@ const experiences = [
     at: 'Sumline, SUP46',
     title: 'Marketing Coordinator',
     subtitle: 'Summer Internship',
+    url: 'http://sup46.com',
     remarks: [
-      'Marketing strategies for a startup company',
-      'Marketing strategies to launch a media application',
+      'Marketing strategies for startup',
+      'Media consumption'
     ]
   },
   {
@@ -22,12 +23,14 @@ const experiences = [
     to: '2014',
     at: 'THS, Tekniska Högskolans Studentkår',
     title: 'President',
-    subtitle: 'Full time',
+    subtitle: '',
     url: 'http://www.ths.kth.se/',
     remarks: [
       'Managing director',
-      'Operational development',
-      'Student representation'
+      'Leadership',
+      'Operational and organizational development',
+      'Student representation',
+      'Highter education politics'
     ]
   },
   {
@@ -57,6 +60,46 @@ const experiences = [
       'Knowledge transformation'
     ]
   },
+  {
+    type: 'WORK',
+    from: '2010',
+    to: '2013',
+    at: 'Mattecoach på nätet',
+    title: 'Coach',
+    subtitle: '',
+    url: 'http://www.mattecoach.se/#/about',
+    remarks: [
+      'E-learning',
+      'Mathematics',
+      'Pedagogics'
+    ]
+  },
+  {
+    type: 'WORK',
+    from: '2014',
+    to: '2014',
+    at: 'Upgrades',
+    title: 'Teacher',
+    subtitle: 'Summer Internship',
+    url: 'http://www.upgrades.se',
+    remarks: [
+      'Study technique',
+      'High school pedagogics'
+    ]
+  },
+  {
+    type: 'WORK',
+    from: '2015',
+    at: 'TruePoint Sweden AB',
+    title: 'Master Thesis Student',
+    subtitle: '',
+    url: 'http://www.truepoint.com/',
+    remarks: [
+      'Organizational development',
+      'Leadership development',
+      'IT development'
+    ]
+  },
   // ************ EDUCATION ************
   {
     type: 'EDUCATION',
@@ -84,8 +127,147 @@ const experiences = [
       'Education'
     ]
   },
+  {
+    type: 'EDUCATION',
+    from: '2013',
+    to: '2013',
+    at: 'Udayana University',
+    title: 'Bali International Program on Asian Studies',
+    subtitle: '',
+    url: 'http://www.asiaexchange.org/en/study-abroad-programs/study-in-bali',
+    remarks: [
+      'Language: Indonesian',
+      'South East Asian Economy'
+    ]
+  },
+  {
+    type: 'EDUCATION',
+    from: '2005',
+    to: '2008',
+    at: 'Fredrika Bremergymnasiet',
+    title: 'Natural Sciences',
+    subtitle: '',
+    url: 'fredrikabremergymnasierna.se',
+    remarks: [
+      'Specializing in Mathematics',
+      'Languages: Spanish, English'
+    ]
+  },
   // ************ COMMISIONS OF TRUST ************
-
+  {
+    type: 'COMMISSION',
+    from: '2013',
+    to: '2015',
+    at: 'KTH Royal Institute of Technology',
+    title: 'Alumni Advisory Board',
+    subtitle: '',
+    url: 'https://www.kth.se/en/opportunities/',
+    remarks: [
+      'KTH Opportunities Fund'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2013',
+    to: '2015',
+    at: 'KTH Royal Institute of Technology',
+    title: 'University Board',
+    subtitle: '',
+    url: 'https://www.kth.se/en/om/organisation/universitetsstyrelsen',
+    remarks: [
+      'KTH highest decision making body'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2013',
+    to: '2015',
+    at: 'SSCO Stockholm Studentkårers Centralorganisation',
+    title: 'Board of Directors',
+    subtitle: '',
+    url: 'http://www.ssco.se/en/',
+    remarks: [
+      'Stockholm housing politics',
+      'Organizational development',
+      'Economic governance'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2014',
+    to: '2015',
+    at: 'THS Tekniska Högskolans Studentkår',
+    title: 'Chairman, Board of Directors',
+    subtitle: '',
+    url: 'http://www.ths.kth.se',
+    remarks: [
+      'Organizational structures',
+      'Strategic development',
+      'Monitoring of operations'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2011',
+    to: '2012',
+    at: 'THS Local Chapter for Master of Science and of Education',
+    title: 'Chairman of the Chapter',
+    subtitle: '',
+    url: 'http://www.cl-sektionen.se',
+    remarks: [
+      'Organizational development'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2012',
+    to: '2012',
+    at: 'THS Local Chapter for Master of Science and of Education',
+    title: 'Vice Chairman of the International Board',
+    subtitle: '',
+    url: 'http://www.cl-sektionen.se',
+    remarks: [
+      'Internationalization of the chapter'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2011',
+    to: '2011',
+    at: 'THS Local Chapter for Master of Science and of Education',
+    title: 'Vice Chairman of the Reception Board',
+    subtitle: '',
+    url: 'http://www.cl-sektionen.se',
+    remarks: [
+      'Project management'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2010',
+    to: '2011',
+    at: 'THS Local Chapter for Master of Science and of Education',
+    title: 'Vice Chairman of the Social Welfare Board',
+    subtitle: '',
+    url: 'http://www.cl-sektionen.se',
+    remarks: [
+      'Event planning'
+    ]
+  },
+  {
+    type: 'COMMISSION',
+    from: '2004',
+    to: '2012',
+    at: 'Lsterhaninge Båtklubbs Ungdomssektion',
+    title: 'Sailing Instructor and Board member',
+    subtitle: '',
+    url: 'http://www.obk.se',
+    remarks: [
+      'Group dynamics',
+      'Leadership',
+      'Project planning'
+    ]
+  }
   // ************ OTHER ************
  
 ];
